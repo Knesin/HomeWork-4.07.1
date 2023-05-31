54ae784d4b13acb8640015e2760239ded1c45046
Maksim Kl <GuruHacker@yandex.ru>
2023-05-31 15:43:59 +0300

    Initial commit

---
+++ HomeWork-4.07.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print_vi(const std::vector<int>& vi)
+{
+    for (const auto& i : vi) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> vi = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    print_vi(vi);
+    std::sort(vi.begin(), vi.end());
+    auto un = std::unique(vi.begin(), vi.end());
+    vi.erase(un, vi.end());
+    std::cout << "[OUT]: ";
+    print_vi(vi);
+}
